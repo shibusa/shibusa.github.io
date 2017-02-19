ded8f9108d18244f8b8fe74a578e455264f93dc4
Shirfeng Chang <shirfeng.chang@gmail.com>
2017-02-19 15:41:29 -0800

    split partials

--- static/routes.js
+++ static/routes.js
@@ -2,10 +2,8 @@ var app = angular.module('shibusa', ['ngRoute'])
 
 app.config(function($routeProvider){
   $routeProvider
-  .when('/', {
-    templateUrl: 'partials/projects.html'
-  })
-  .otherwise({
-    redirectTo: '/'
-  })
+  .when('/projects', {templateUrl: 'partials/projects.html'})
+  .when('/tools', {templateUrl: 'partials/tools.html'})
+  .when('/others', {templateUrl: 'partials/others.html'})
+  .otherwise({redirectTo: '/'})
 })
